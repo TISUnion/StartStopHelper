0e6cbed736e2e1d4a57fa437ffbebeb0703afcf9
TRCYX <2915154295@qq.com>
2020-04-15 18:04:46 +0800

    Filter commands to reduce spam.

--- StartStopHelper.py
+++ StartStopHelper.py
@@ -11,26 +11,39 @@ def wait(server, command, time_wait):
 		time_wait -= 1
 
 
+CMD_PREFIX = '!!'
+RECOGNIZED_CMDS = [
+	'start',
+	'stop',
+	'stop_exit',
+	'restart'
+]
+
+
 def on_info(server, info):
 	if info.is_user:
+		args = info.content.split(' ')
+		if len(args) not in [1, 2] or not args[0].startswith(CMD_PREFIX):
+			return
+
+		command = args[0][len(CMD_PREFIX):]
+		if command not in RECOGNIZED_CMDS:
+			return
+
 		if server.get_permission_level(info) >= 3:  # admin
-			args = info.content.split(' ')
-			if len(args) not in [1, 2]:
-				return
 			if len(args) == 2 and not args[1].isdigit():
 				server.reply(info, '[<time_wait>] argument need to be a number')
 				return
-			command = args[0]
 			time_wait = int(args[1]) if len(args) == 2 else 10
-			if command == '!!start':
+			if command == 'start':
 				server.start()
-			elif command == '!!stop':
+			elif command == 'stop':
 				wait(server, 'stop', time_wait)
 				server.stop()
-			elif command == '!!stop_exit':
+			elif command == 'stop_exit':
 				wait(server, 'stop', time_wait)
 				server.stop_exit()
-			elif command == '!!restart':
+			elif command == 'restart':
 				wait(server, 'restart', time_wait)
 				server.restart()
 		else:
