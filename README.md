b59d612474ba2fe4d1e0360c13f5c641f331b0aa
zhang-anzhi <zhang_anzhi@126.com>
2020-07-16 17:04:09 +0800

    add exit to support high version MCDR and add abort

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ For permission level = `admin`
 
 `!!stop_exit [<time_wait>]`: stop the server and exit MCDR
 
+`!!exit`: exit MCDR
+
 `[<time_wait>]` is an optional argument. if not set it's value is `10`. MCDR will count down for `time_wait` second to execute the command
 
 For reducing `!!help` message and since normal player won't use this command I didn't add help message
--- StartStopHelper.py
+++ StartStopHelper.py
@@ -2,49 +2,51 @@
 
 import time
 
-
-def wait(server, command, time_wait):
-	while time_wait > 0:
-		if server.is_server_startup():
-			server.say('§cServer will {} in {}s§r'.format(command, time_wait))
-		time.sleep(1)
-		time_wait -= 1
-
-
-CMD_PREFIX = '!!'
-RECOGNIZED_CMDS = [
-	'start',
-	'stop',
-	'stop_exit',
-	'restart'
-]
+def action(server, command, time_wait):
+    for countdown in range(0, time_wait):
+        server.say('§cServer will {} in {} second! Use !!abort to abort'.format(time_wait - countdown, command))
+        for i in range(10):
+            time.sleep(0.1)
+            global abort
+            if abort:
+                server.say('§cAction aborted')
+                return False
+    else:
+        return True
 
 
-def on_info(server, info):
-	if info.is_user:
-		args = info.content.split(' ')
-		if len(args) not in [1, 2] or not args[0].startswith(CMD_PREFIX):
-			return
+def on_user_info(server, info):
+    if info.content.rstrip(' ') == '!!abort':
+        global abort
+        abort = True
 
-		command = args[0][len(CMD_PREFIX):]
-		if command not in RECOGNIZED_CMDS:
-			return
+    if info.content.startswith('!!') and info.is_user:
+        args = info.content.split(' ')
+        if len(args) not in [1, 2]:
+            return
 
-		if server.get_permission_level(info) >= 3:  # admin
-			if len(args) == 2 and not args[1].isdigit():
-				server.reply(info, '[<time_wait>] argument need to be a number')
-				return
-			time_wait = int(args[1]) if len(args) == 2 else 10
-			if command == 'start':
-				server.start()
-			elif command == 'stop':
-				wait(server, 'stop', time_wait)
-				server.stop()
-			elif command == 'stop_exit':
-				wait(server, 'stop', time_wait)
-				server.stop_exit()
-			elif command == 'restart':
-				wait(server, 'restart', time_wait)
-				server.restart()
-		else:
-			server.reply(info, '§cPermission denied§r')
+        command = args[0][2:]
+        if command not in ['start', 'stop', 'stop_exit', 'restart', 'exit']:
+            return
+
+        if server.get_permission_level(info) >= 3:
+            abort = False
+            if len(args) == 2 and not args[1].isdigit():
+                server.reply(info, '§c[time] argument need to be a number')
+                return
+            time_wait = int(args[1]) if len(args) == 2 else 0
+            if command == 'start':
+                server.start()
+            elif command == 'stop':
+                if action(server, 'stop', time_wait):
+                    server.stop()
+            elif command == 'stop_exit':
+                if action(server, 'stop', time_wait):
+                    server.stop_exit()
+            elif command == 'restart':
+                if action(server, 'restart', time_wait):
+                    server.restart()
+            elif command == 'exit':
+                server.exit()
+        else:
+            server.reply(info, '§cPermission denied')
