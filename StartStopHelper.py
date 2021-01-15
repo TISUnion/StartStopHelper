bd1f8537985e5edd8aa58fd91ca35ef5461fc1bf
Fallen_Breath <yqx791125833@gmail.com>
2021-01-16 00:05:46 +0800

    MCDR v1.0 support

--- StartStopHelper.py
+++ StartStopHelper.py
@@ -1,12 +1,23 @@
-# -*- coding: utf-8 -*-
-
 import time
+
+from mcdreforged.api.all import *
+
+PLUGIN_METADATA = {
+	'id': 'start_stop_helper',
+	'version': '1.0.0',
+	'name': 'Start Stop Helper',
+	'description': 'Help admin to start / stop / restart the server',
+	'author': 'Fallen_Breath',
+	'link': 'https://github.com/MCDReforged/StartStopHelper'
+}
 
 
 def wait(server, command, time_wait):
 	while time_wait > 0:
+		msg = '§cServer will {} in {}s§r'.format(command, time_wait)
 		if server.is_server_startup():
-			server.say('§cServer will {} in {}s§r'.format(command, time_wait))
+			server.say(msg)
+		server.logger.info(msg)
 		time.sleep(1)
 		time_wait -= 1
 
@@ -20,16 +31,19 @@ RECOGNIZED_CMDS = [
 ]
 
 
-def on_info(server, info):
-	if info.is_user:
-		args = info.content.split(' ')
-		if len(args) not in [1, 2] or not args[0].startswith(CMD_PREFIX):
-			return
+def on_user_info(server, info: Info):
+	args = info.content.split(' ')
+	if len(args) not in [1, 2] or not args[0].startswith(CMD_PREFIX):
+		return
 
-		command = args[0][len(CMD_PREFIX):]
-		if command not in RECOGNIZED_CMDS:
-			return
+	command = args[0][len(CMD_PREFIX):]
+	if command not in RECOGNIZED_CMDS:
+		return
 
+	info.cancel_send_to_server()
+
+	@new_thread(PLUGIN_METADATA['name'])
+	def inner():
 		if server.get_permission_level(info) >= 3:  # admin
 			if len(args) == 2 and not args[1].isdigit():
 				server.reply(info, '[<time_wait>] argument need to be a number')
@@ -48,3 +62,5 @@ def on_info(server, info):
 				server.restart()
 		else:
 			server.reply(info, '§cPermission denied§r')
+
+	inner()
